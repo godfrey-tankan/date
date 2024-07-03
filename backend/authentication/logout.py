9b02a8adebc4f75b48a6ad2c2950ae45adeea703
godfrey-tankan <godfreytantswakandeya@gmail.com>
2024-07-03 10:42:02 +0200

    refactor: Add logout functionality and route for authentication

---
+++ backend/authentication/logout.py
@@ -0,0 +1,12 @@
+from django.contrib.auth.views import LogoutView
+from django.http import HttpResponseRedirect
+from django.urls import reverse_lazy
+from django.contrib.auth import logout
+
+class CustomLogoutView(LogoutView):
+    print("CustomLogoutView.............")
+    next_page = reverse_lazy('home')
+
+    def dispatch(self, request, *args, **kwargs):
+        logout(request)
+        return super().dispatch(request, *args, **kwargs)
--- backend/authentication/urls.py
+++ backend/authentication/urls.py
@@ -3,6 +3,7 @@ from . import views
 from . import user_profile_info
 from .login import UserAuthenticationAPIView
 from django.urls import path, include
+from .logout import CustomLogoutView
 
 
 urlpatterns = [
@@ -10,6 +11,7 @@ urlpatterns = [
     path('', UserAuthenticationAPIView.as_view(), name='login'),
     path('home/', views.home_view, name='home'),
     path('profile/', user_profile_info.get_profile_data, name='profile'),
+    path('logout/', CustomLogoutView.as_view(), name='logout'),
 
 
 ]
